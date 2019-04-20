2b64f898911ed567ab2aa4cd3fcba7164ef5c2f8
shiaoyi <shiaoyi0913@gmail.com>
2019-04-20 18:21:23 +0800

    Update Items.js

--- models/Items.js
+++ models/Items.js
@@ -2,10 +2,10 @@ module.exports = function(sequelize, DataTypes){
 	var Item = sequelize.define('Item',{
 		item: DataTypes.STRING,
 		price: DataTypes.STRING,
-        num: DataTypes.STRING,
-        importdate: DataTypes.DATE,
-        duedate: DataTypes.DATE,
-        promoteday: DataTypes.TIME
+		num: DataTypes.STRING,
+		importdate: DataTypes.DATE,
+		duedate: DataTypes.DATE,
+		promoteday: DataTypes.TIME
 	});
 	return Item;
 };
