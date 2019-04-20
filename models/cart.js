b731aa1f88834f0dee359b5fc438fdba42b130a1
shiaoyi <shiaoyi0913@gmail.com>
2019-04-20 18:22:01 +0800

    Update cart.js

--- models/cart.js
+++ models/cart.js
@@ -2,7 +2,7 @@ module.exports = function(sequelize, DataTypes){
 	var Cart = sequelize.define('Cart',{
 		product: DataTypes.STRING,
 		price: DataTypes.INTEGER,
-        num: DataTypes.INTEGER,
+        	num: DataTypes.INTEGER,
 	});
 	return Cart;
 };
